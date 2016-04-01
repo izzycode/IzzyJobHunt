69050129498701576356faed61ad974b77701d52
marcemc <marcemc@yahoo.com>
2016-03-31 23:05:11 -0400

    run migrations. created schema.

---
+++ db/schema.rb
@@ -0,0 +1,80 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160331203505) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "attempts", force: :cascade do |t|
+    t.datetime "date"
+    t.string   "medium"
+    t.text     "summary"
+    t.integer  "job_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "attempts", ["job_id"], name: "index_attempts_on_job_id", using: :btree
+  add_index "attempts", ["user_id"], name: "index_attempts_on_user_id", using: :btree
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "main_phone"
+    t.text     "useful_links"
+    t.text     "notes"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "honchos", force: :cascade do |t|
+    t.string   "name"
+    t.string   "position"
+    t.string   "phone_number"
+    t.string   "email"
+    t.text     "notes"
+    t.text     "links"
+    t.integer  "company_id"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "honchos", ["company_id"], name: "index_honchos_on_company_id", using: :btree
+  add_index "honchos", ["user_id"], name: "index_honchos_on_user_id", using: :btree
+
+  create_table "jobs", force: :cascade do |t|
+    t.string   "position"
+    t.string   "web_address"
+    t.integer  "user_id"
+    t.integer  "company_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "jobs", ["company_id"], name: "index_jobs_on_company_id", using: :btree
+  add_index "jobs", ["user_id"], name: "index_jobs_on_user_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+
+end
