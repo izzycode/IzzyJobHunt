9278c14ed5fff877ddc50f04c632ee2489a74d04
marcemc <marcemc@yahoo.com>
2016-04-08 10:48:17 -0400

    refactored companies index into partial

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   def many_honchos
-    if Honcho.all.count > 0
-      @hochos = 0
-      self.jobs.each do |h|
-        @honchos += h.company.honchos.count
-        p @honchos
-      end
-    else
-      "0"
-    end
+    
   end
 
 
