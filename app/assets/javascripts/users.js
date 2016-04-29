d5c4b4a55825b5b946f2b973307d0a45cbd59c6d
marcemc <marcemc@yahoo.com>
2016-04-29 09:35:02 -0400

    Add indeed number to ENV

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,13 +1,25 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-// $('#new_company').trigger('submit.rails').empty();
-// $(document).ready(function(){
-//     $("#new_company").trigger(function(event){
-//       event.preventDefault();
-//       self.trigger("submit.rails");
-//       self.empty();
-//     });
-// });
 
-// $('#new_company').hide.trigger('submit.rails');
+var ind_pub = ENV['INDEED_NUMBER'];
+var ind_el = 'indJobContent';
+var ind_pf = '';
+var ind_q = 'Web Developer';
+var ind_l = 'south florida';
+var ind_chnl = 'web';
+var ind_n = 6;
+var ind_d = 'http://www.indeed.com';
+var ind_t = 50;
+var ind_c = 30;
+var ind_pgn = 1;
+var ind_pgnCnt = 3;
+
+function clearDefaults() {
+  var formInputs = document.getElementById('indeed_jobform').elements;
+  for(var i = 0; i < formInputs.length; i++) {
+    if(formInputs[i].value == 'title, keywords' || formInputs[i].value == 'city, state, or zip') {
+      formInputs[i].value = '';
+    }
+  }
+}
