ffbdfbd0f0171e9aa038b2b41557e89dbe1cf952
marcemc <marcemc@yahoo.com>
2016-05-08 11:26:23 -0400

    May have solved the mixed content issue with indeed responses

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
   end
 
   def create
-    byebug
 
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
@@ -16,7 +15,7 @@ class SessionsController < ApplicationController
       render 'welcome/izzybam'
     end
 
-    quote = HTTParty.get "https://quotes.rest/qod.json?category=inspire"
+    quote = HTTParty.get "//quotes.rest/qod.json?category=inspire"
     result = (JSON.parse quote.body)["contents"]["quotes"][0]
     session[:quote] = result["quote"]
     session[:image] = result["background"]
